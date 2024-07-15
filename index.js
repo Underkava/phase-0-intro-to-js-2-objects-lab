fa59084189a3b24077fa9731e24780ff98689387
Samuel Kinyua <samwakash@gmail.com>
2024-07-15 20:55:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+// index.js
+
+// Function 1: Non-destructively updates the employee object
+let employee = {
+    name: "Samuel",
+    streetAddress: "ngong road"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+// Function 2: Non-destructively updates the employee object
+function updateObjectWithKeyAndValue(object, key, value) {
+    return {
+        ...object,
+        [key]: value
+    };
+}
